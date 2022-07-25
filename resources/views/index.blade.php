c398dd660295e7f67afcfb764b0085a4957341c4
evame94 <katerinka.melnichuk@gmail.com>
2022-07-25 16:08:30 +0200

    Finished project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="/img/ekaterina_melnichuk.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Ekaterina's portfolio page summarizing her experience, education and skills as a web developer"
+    />
+    <meta
+      name="author"
+      content="Ekaterina Melnichuk"
+    />
+    <link rel="stylesheet" href="{{mix("css/style.css")}}">
+    <link href="//db.onlinewebfonts.com/c/0e979bd4a3c1c6ac788ed57ac569667f?family=revicons" rel="stylesheet" type="text/css"/>
+    <script
+      src="https://kit.fontawesome.com/5f0205c008.js"
+      crossorigin="anonymous"
+    ></script>
+    <title>Ekaterina's portfolio page</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+    <!--
+      This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the <body> tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build`.
+    -->
+    <script src="{{mix('js/index.js')}}"></script>
+  </body>
+</html>
